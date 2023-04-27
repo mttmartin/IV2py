9c8c7c1239690c358ea99157262e07abc0b7d8b1
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-04-27 21:33:31 +0200

    doc: add license file

--- src/iv2py/sam.cpp
+++ src/iv2py/sam.cpp
@@ -1,3 +1,9 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2023, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2023, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file.
+// -----------------------------------------------------------------------------------------------------
 #include "record_reader.h"
 
 #include <ivio/ivio.h>
@@ -8,7 +14,7 @@ namespace py = pybind11;
 void init_sam_mod(py::module& parent_mod) {
     auto mod = parent_mod.def_submodule("sam");
 
-    // Provding the record class
+    // Providing the record class
     py::class_<ivio::sam::record>(mod, "record")
         .def(py::init<>([](std::string qname,
                            int32_t     flag,
