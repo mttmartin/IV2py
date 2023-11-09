1c9bbf4c4d6374d1e61741247b9752a78bad6443
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-11-09 18:15:13 +0100

    feat: spdx headers and reuse CI

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+<!--
+    SPDX-FileCopyrightText: 2006-2023, Knut Reinert & Freie Universität Berlin
+    SPDX-FileCopyrightText: 2016-2023, Knut Reinert & MPI für molekulare Genetik
+    SPDX-License-Identifier: CC-BY-4.0
+-->
+
 # IV2py
 
 Wrapper code to bring libraries from the iv-project to python, currently supported are:
--- python-bindings/build.sh
+++ python-bindings/build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
+# SPDX-FileCopyrightText: 2006-2023, Knut Reinert & Freie Universität Berlin
+# SPDX-FileCopyrightText: 2016-2023, Knut Reinert & MPI für molekulare Genetik
+# SPDX-License-Identifier: CC0-1.0
+
 cd "$(dirname "$0")"
 
 docker run -u $(id -u) -ti --rm -e PLAT="manylinux_2_28_x86_64" -v $(pwd)/..:/code quay.io/pypa/manylinux_2_28_x86_64 /code/python-bindings/build-wheels.sh
