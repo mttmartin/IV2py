4a4fcb96d63cb9b4ab2b85a72fe7e79444652e7c
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-11-26 18:27:20 +0100

    feat: using CPM

---
+++ src/iv2py/fmindex.cpp
@@ -0,0 +1,56 @@
+// SPDX-FileCopyrightText: 2006-2023, Knut Reinert & Freie Universität Berlin
+// SPDX-FileCopyrightText: 2016-2023, Knut Reinert & MPI für molekulare Genetik
+// SPDX-License-Identifier: BSD-3-Clause
+#include <filesystem>
+#include <fmindex-collection/fmindex-collection.h>
+#include <fstream>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <sstream>
+
+#include <cereal/archives/binary.hpp>
+#include <cereal/types/array.hpp>
+#include <cereal/types/vector.hpp>
+
+namespace py = pybind11;
+namespace fmc = fmindex_collection;
+
+void init_fmindex_mod(py::module& mod) {
+//    auto mod = parent_mod.def_submodule("fmindex");
+
+    using Index = fmc::VariableFMIndex;
+
+    // Providing the fmindex class
+    py::class_<Index>(mod, "fmindex")
+        .def(py::init<>([](std::vector<std::string> reference, size_t samplingRate, size_t threadNbr) {
+            return Index { reference, samplingRate, threadNbr };
+        }), py::arg("reference") = std::vector<std::string>{}, py::arg("samplingRate") = 16, py::arg("threadNbr") = 1)
+        .def(py::init<>([](std::filesystem::path path) {
+            auto index = Index{};
+            auto ifs     = std::ifstream{path, std::ios::binary};
+            auto archive = cereal::BinaryInputArchive{ifs};
+            archive(index);
+            return index;
+        }), py::arg("path"))
+        .def("search", [](Index const& index, std::string const& query) -> std::vector<std::tuple<size_t, size_t>> {
+            return index.search(query);
+        }, py::arg("query"))
+        .def("load", [](Index& index, std::filesystem::path path) {
+            auto ifs     = std::ifstream{path, std::ios::binary};
+            auto archive = cereal::BinaryInputArchive{ifs};
+            archive(index);
+        }, py::arg("path"))
+        .def("save", [](Index& index, std::filesystem::path path) {
+            auto ofs     = std::ofstream{path, std::ios::binary};
+            auto archive = cereal::BinaryOutputArchive{ofs};
+            archive(index);
+        }, py::arg("path"))
+        .def("__repr__", [](Index const& index) -> std::string {
+            auto ss = std::stringstream{};
+            ss << "<iv2py.fmindex ";
+            ss << "0x" << std::hex << &index;
+            ss << " >";
+            return ss.str();
+        })
+    ;
+}
---
+++ src/iv2py/normalize.cpp
@@ -0,0 +1,48 @@
+// SPDX-FileCopyrightText: 2006-2023, Knut Reinert & Freie Universität Berlin
+// SPDX-FileCopyrightText: 2016-2023, Knut Reinert & MPI für molekulare Genetik
+// SPDX-License-Identifier: BSD-3-Clause
+#include <ivsigma/ivsigma.h>
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+void init_sigma_mod(py::module& parent_mod) {
+    auto mod = parent_mod.def_submodule("sigma");
+
+    // Providing the record class
+    py::class_<ivio::fasta::record>(mod, "record")
+        .def(py::init<>([](std::string id, std::string seq) {
+            return ivio::fasta::record {
+                .id  = std::move(id),
+                .seq = std::move(seq),
+            };
+        }), py::arg("id") = "", py::arg("seq") = "")
+        .def_readwrite("id", &ivio::fasta::record::id)
+        .def_readwrite("seq", &ivio::fasta::record::seq)
+        .def("__repr__", [](ivio::fasta::record const& o) -> std::string {
+            return "<iv2py.fasta.record id '" + o.id + "', seq '" + std::string{o.seq} + "'>";
+        })
+    ;
+
+    // Providing the reader class
+    py::class_<record_reader<ivio::fasta::reader>>(mod, "reader")
+        .def(py::init([](std::string const& path) {
+            return std::make_unique<record_reader<ivio::fasta::reader>>(path);
+        }), py::arg("file"))
+        .def("__iter__", [](record_reader<ivio::fasta::reader>& r) {
+            return py::make_iterator(r.view.begin(), r.view.end());
+        }, py::keep_alive<0, 1>())
+    ;
+
+    // Providing the writer class
+    py::class_<ivio::fasta::writer>(mod, "writer")
+        .def(py::init([](std::string const& path) {
+            return std::make_unique<ivio::fasta::writer>(ivio::fasta::writer::config{path});
+        }), py::arg("file"))
+        .def("write", [](ivio::fasta::writer& writer, ivio::fasta::record const& record) {
+            writer.write(record);
+        }, py::arg("record"))
+        .def("close", [](ivio::fasta::writer& writer) {
+            writer.close();
+        })
+    ;
+}
